63f2611faa5f60e787b9ee5ef88ab94f5b5747c3
Laurence Rawlings <contact@laurencerawlings.com>
2021-12-25 19:39:56 +0000

    Add sidebar view

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -1,9 +1,16 @@
 import * as vscode from 'vscode';
+import { SidebarProvider } from './SidebarProvider';
 
 export function activate(context: vscode.ExtensionContext) {
+	const sidebarProvider = new SidebarProvider(context.extensionUri);
+
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider("deversus-sidebar", sidebarProvider)
+	);
+
 	context.subscriptions.push(vscode.commands.registerCommand('deversus.helloWorld', () => {
 		vscode.window.showInformationMessage('Hello World from Deversus!');
 	}));
 }
 
-export function deactivate() {}
+export function deactivate() { }
