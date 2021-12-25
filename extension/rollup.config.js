121cf56ff6486df5be28249d4589743649ffd4bf
Laurence Rawlings <contact@laurencerawlings.com>
2021-12-25 19:02:06 +0000

    Add svelete

---
+++ extension/rollup.config.js
@@ -0,0 +1,50 @@
+import svelte from "rollup-plugin-svelte";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+import sveltePreprocess from "svelte-preprocess";
+import typescript from "@rollup/plugin-typescript";
+import path from "path";
+import fs from "fs";
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default fs
+  .readdirSync(path.join(__dirname, "src", "webviews", "pages"))
+  .map((input) => {
+    const name = input.split(".")[0];
+    return {
+      input: "src/webviews/pages/" + input,
+      output: {
+        sourcemap: true,
+        format: "iife",
+        name: "app",
+        file: "out/compiled/" + name + ".js",
+      },
+      plugins: [
+        svelte({
+          dev: !production,
+          css: (css) => {
+            css.write(name + ".css");
+          },
+          preprocess: sveltePreprocess(),
+        }),
+
+        resolve({
+          browser: true,
+          dedupe: ["svelte"],
+        }),
+        commonjs(),
+        typescript({
+          tsconfig: "src/webviews/tsconfig.json",
+          sourceMap: !production,
+          inlineSources: !production,
+        }),
+
+        production && terser(),
+      ],
+      watch: {
+        clearScreen: false,
+      },
+    };
+  });
